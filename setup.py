9a896e5949a7da53d34ac9340986e8ea68c5f31d
Jarkko Piiroinen <jmp@git.mail.kapsi.fi>
2019-01-26 13:22:00 +0200

    Add classifiers to setup.py.

--- setup.py
+++ setup.py
@@ -3,11 +3,17 @@
 from distutils.core import setup
 
 setup(
-    name='farbfeld',
-    version='0.1.0',
-    description='Loader for the farbfeld image format.',
-    author='Jarkko Piiroinen',
-    author_email='jmp@python.mail.kapsi.fi',
-    url='https://github.com/jmp/farbfeld',
-    py_modules=['farbfeld'],
+    name="farbfeld",
+    version="0.1.0",
+    description="Loader for the farbfeld image format.",
+    author="Jarkko Piiroinen",
+    author_email="jmp@python.mail.kapsi.fi",
+    url="https://github.com/jmp/farbfeld",
+    py_modules=["farbfeld"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Multimedia :: Graphics",
+    ],
 )
