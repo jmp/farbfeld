edfa1694540c5cdc6cfc76ce269045a1cc86ba81
Jarkko Piiroinen <jmp@git.mail.kapsi.fi>
2019-01-26 13:31:12 +0200

    Add long description from README.md.

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python
 
+from os import path
 from distutils.core import setup
+
+
+# Read long description from README.md
+root_dir = path.abspath(path.dirname(__file__))
+with open(path.join(root_dir, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(
     name="farbfeld",
     version="0.1.0",
     description="Loader for the farbfeld image format.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author="Jarkko Piiroinen",
     author_email="jmp@python.mail.kapsi.fi",
     url="https://github.com/jmp/farbfeld",
